8ff5553d4d9354a4d5cbc1228d679b915e6441cb
Nico Garbaccio <garbaccio20@gmail.com>
2025-06-09 14:30:16 -0400

    chore: update service worker after build

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-4e60344b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.jjk69vtl4ao"
+    "revision": "0.fghusl1tor8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
